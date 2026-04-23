dd37c27bad5e41c0e92615fca6eaf159e6592b1c
shyshkovskaliliia-art <shyshkovska.liliia@chnu.edu.ua>
2026-04-23 10:56:55 +0300

    Update FacultyEventArgs with EventPriority and Timestamp
    Refactor FacultyEventArgs to use EventPriority enum and add Timestamp.

--- Lab9_10CharpT/Models/FacultyEvents.cs
+++ Lab9_10CharpT/Models/FacultyEvents.cs
@@ -1,20 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 namespace FacultyLife.Models
 {
     public delegate void FacultyEventHandler(object sender, FacultyEventArgs e);
 
+    public enum EventPriority
+    {
+        High = 3,
+        Medium = 2,
+        Low = 1
+    }
+
     public class FacultyEventArgs : EventArgs
     {
         public string ActivityName { get; }
-        public int Priority { get; }
-        public string Result { get; set; }
+        public EventPriority Priority { get; }
+        public DateTime Timestamp { get; }
 
-        public FacultyEventArgs(string name, int priority)
+        public string DeanResult { get; set; } = string.Empty;
+
+        public List<string> StudentResults { get; } = new List<string>();
+
+        public FacultyEventArgs(string name, EventPriority priority)
         {
             ActivityName = name;
-            this.Priority = priority;
+            Priority = priority;
+            Timestamp = DateTime.Now;
         }
     }
 }
-
