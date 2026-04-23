f8a1b63a00502ca923415641d60992711f75a973
shyshkovskaliliia-art <shyshkovska.liliia@chnu.edu.ua>
2026-04-23 10:56:28 +0300

    update
    Refactor event handling and simulate life logic in Faculty class.

--- Lab9_10CharpT/Models/Faculty.cs
+++ Lab9_10CharpT/Models/Faculty.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace FacultyLife.Models
@@ -8,28 +9,55 @@ namespace FacultyLife.Models
     {
         public event FacultyEventHandler DayOfFaculty;
 
+        private readonly List<FacultyEventArgs> _eventQueue = new();
+
+        public FacultyStatistics Statistics { get; } = new();
+
         protected virtual void OnDayOfFaculty(FacultyEventArgs e)
         {
-            if (DayOfFaculty != null)
-            {
-                DayOfFaculty(this, e);
-            }
+            DayOfFaculty?.Invoke(this, e);
         }
 
-        public async Task SimulateLife(IProgress<FacultyEventArgs> progress)
+        public void EnqueueEvents()
         {
-            string[] activities = { "Урочиста частина", "Спортивні змагання", "Концерт" };
-            Random rnd = new Random();
+            _eventQueue.Clear();
+            _eventQueue.Add(new FacultyEventArgs("Урочиста частина", EventPriority.High));
+            _eventQueue.Add(new FacultyEventArgs("Спортивні змагання", EventPriority.Medium));
+            _eventQueue.Add(new FacultyEventArgs("Концерт", EventPriority.Medium));
+            _eventQueue.Add(new FacultyEventArgs("Виставка студентських робіт", EventPriority.Low));
+            _eventQueue.Add(new FacultyEventArgs("Зустріч із випускниками", EventPriority.High));
+            _eventQueue.Add(new FacultyEventArgs("Майстер-клас", EventPriority.Low));
+        }
 
-            foreach (var act in activities)
+        public async Task SimulateLife(IProgress<FacultyEventArgs> progress,
+                                       IProgress<string> statusProgress)
+        {
+            var ordered = _eventQueue.OrderByDescending(e => (int)e.Priority).ToList();
+
+            Statistics.StartTime = DateTime.Now;
+            Statistics.TotalEvents = ordered.Count;
+
+            foreach (var e in ordered)
             {
-                await Task.Delay(1000);
-                var e = new FacultyEventArgs(act, rnd.Next(1, 3));
+                statusProgress.Report($"Виконується: {e.ActivityName}...");
 
+                int delay = e.Priority switch
+                {
+                    EventPriority.High => 700,
+                    EventPriority.Medium => 1000,
+                    EventPriority.Low => 1400,
+                    _ => 1000
+                };
+
+                await Task.Delay(delay);
                 OnDayOfFaculty(e);
+                Statistics.Record(e);
+
                 progress.Report(e);
             }
+
+            Statistics.EndTime = DateTime.Now;
+            statusProgress.Report("Готово");
         }
     }
 }
-
